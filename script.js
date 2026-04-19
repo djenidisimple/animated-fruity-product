7355d67de8370fffad51bdb027e3025b90655d50
djenidisimple <djenidigauss@gmail.com>
2026-04-19 21:25:43 +0300

    new feature

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const can = document.querySelectorAll('.can');
+const background = document.querySelector('.background');
 can[0].style.display = 'block';
 const buttons = document.querySelectorAll('button');
 let currentIndex = 0;
 buttons[0].addEventListener('click', () => {
     can[currentIndex].style.display = 'none';
     can[currentIndex].classList.remove('animation');
-    currentIndex = (currentIndex + 1) % can.length;
+    currentIndex = (currentIndex - 1 + can.length) % can.length;
     can[currentIndex].style.display = 'block';
     can[currentIndex].classList.add('animation');
+    background.style.background = 'var(--background-' + (currentIndex + 1) + ')';
 });
 buttons[1].addEventListener('click', () => {
     can[currentIndex].style.display = 'none';
     can[currentIndex].classList.remove('animation');
-    currentIndex = (currentIndex - 1 + can.length) % can.length;
+    currentIndex = (currentIndex + 1) % can.length;
     can[currentIndex].style.display = 'block';
     can[currentIndex].classList.add('animation');
+    background.style.background = 'var(--background-' + (currentIndex + 1) + ')';
 });
